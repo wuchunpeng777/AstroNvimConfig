3bc2b77d7cfd2a9fed7b4c823372f7188a68c1da
wuchunpeng <wcpwcp9@gmail.com>
2024-08-06 17:56:29 +0800

    better-escape and treesitter language config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,9 +5,13 @@ return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
     ensure_installed = {
-      "lua",
-      "vim",
       -- add more arguments for adding more treesitter parsers
+      'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc', "c_sharp" 
     },
   },
+  config = function(_, opts)
+    if vim.fn.has "win32" == 1 then
+      require 'nvim-treesitter.install'.compilers = { "zig" }
+    end
+  end
 }
