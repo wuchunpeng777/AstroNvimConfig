ec5e7de8031bf87872023f5aa2291b1e7d600d47
wuchunpeng <wcpwcp9@gmail.com>
2024-08-07 17:08:21 +0800

    C# 反编译

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -10,15 +10,15 @@ return {
   opts = {
     -- Configuration table of features provided by AstroLSP
     features = {
-      codelens = true, -- enable/disable codelens refresh on start
-      inlay_hints = false, -- enable/disable inlay hints on start
+      codelens = true,        -- enable/disable codelens refresh on start
+      inlay_hints = false,    -- enable/disable inlay hints on start
       semantic_tokens = true, -- enable/disable semantic token highlighting
     },
     -- customize lsp formatting options
     formatting = {
       -- control auto formatting on save
       format_on_save = {
-        enabled = false, -- enable or disable format on save globally
+        enabled = true,     -- enable or disable format on save globally
         allow_filetypes = { -- enable format on save for specified filetypes only
           -- "go",
         },
@@ -52,6 +52,18 @@ return {
       -- the key is the server that is being setup with `lspconfig`
       -- rust_analyzer = false, -- setting a handler to false will disable the set up of that language server
       -- pyright = function(_, opts) require("lspconfig").pyright.setup(opts) end -- or a custom handler function can be passed
+      omnisharp = function(_, opts)
+        local config = {
+          handlers = {
+            ["textDocument/definition"] = require('omnisharp_extended').definition_handler,
+            ["textDocument/typeDefinition"] = require('omnisharp_extended').type_definition_handler,
+            ["textDocument/references"] = require('omnisharp_extended').references_handler,
+            ["textDocument/implementation"] = require('omnisharp_extended').implementation_handler,
+          },
+        }
+        opts.handlers = config.handlers
+        require('lspconfig').omnisharp.setup(opts)
+      end
     },
     -- Configure buffer local auto commands to add when attaching a language server
     autocmds = {
